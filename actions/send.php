61d4e79d6b0748a320e271906e2552276c815b3e
Kogan Alexey <koganaleksey@mail.ru>
2023-04-12 20:48:22 +0300

    Automated push via gulp

--- actions/send.php
+++ actions/send.php
@@ -6,14 +6,14 @@ $name = $_POST['name'];
 $email_from = $_POST['email'];
 $message = $_POST['message'];
 $success_message = '<div class="d-flex justify-content-center align-items-center text-center" style="width:100vw;height:100vh">
-<h3 style="width:400px;"><span class="font-weight-bold text-success">Thank you for contacting us!</span><br><br><span class="font-weight-light">We will contact you shortly.<br>You will now be redirected back to<a href="https://aquaduco.io">aquaduco.io.</a></span></h3>
+<h3 style="width:400px;"><span class="font-weight-bold text-success">Thank you for contacting us!</span><br><br><span class="font-weight-light">We will contact you shortly.<br>You will now be redirected back to<a href="https://aquaducouae.com">aquaducouae.com</a></span></h3>
 </div>';
 
 $mail = array(
-  'to' => "koganaleksey@mail.ru",
-  'subject' => "Message from Website aquaduco.io",
+  'to' => "info@aquaducouae.com",
+  'subject' => "Message from aquaducouae.com",
   'message' => "Name: " . $name . "\n\n" . "Email: " . $email_from . "\n\n" . "Message: " . "\r\n" . $message,
-  'headers' => "MIME-Version: 1.0\r\n" . "Content-type: text/plain; charset=utf-8\r\n" . "From: <aquaduco.io>\r\n"
+  'headers' => "MIME-Version: 1.0\r\n" . "Content-type: text/plain; charset=utf-8\r\n" . "From: <aquaducouae.com>\r\n"
 );
 
 mail($mail['to'], $mail['subject'], $mail['message'], iconv('utf-8', 'windows-1251', $mail['headers']));
@@ -23,7 +23,7 @@ echo iconv('utf-8', 'windows-1251', $success_message);
 ?>
 
 <head>
-  <meta http-equiv="refresh" content="7;URL=https://aquaduco.io">
+  <meta http-equiv="refresh" content="7;URL=https://aquaducouae.com">
   <link rel="stylesheet" href="../assets/css/main.min.css">
 </head>
 
